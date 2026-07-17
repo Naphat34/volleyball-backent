7d84e84addfadc44f922293b0b7a65cce93e6d23
Naphat34 <naphatsrirak@gmail.com>
2026-07-17 13:15:28 +0700

    Fix: Add SSL configuration for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,12 +8,17 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    
 
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
+    
+    charset: "utf8mb4",
 
-    charset: "utf8mb4"
+    ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 function normalizeSql(sql) {
